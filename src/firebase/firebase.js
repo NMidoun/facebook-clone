d02cbe1b3bdf471a329a28d8845c6cca7580f9b1
Nabil MIDOUN <nabil.midoun@gmail.com>
2022-06-22 21:07:18 +0200

    setting dependencies and folder structure

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCH9UIMchmEjJcj0PYYzBynrNUPp_yqi9g",
+  authDomain: "facebook-clone-3b5a7.firebaseapp.com",
+  projectId: "facebook-clone-3b5a7",
+  storageBucket: "facebook-clone-3b5a7.appspot.com",
+  messagingSenderId: "299566879663",
+  appId: "1:299566879663:web:1df17f3d7fbb887bb5d55a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
